8707a2acdd19d11ecade6ba2fad0523c229cac3b
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-12-08 19:38:28 -0600

    Estructura General en C
    Código de la estructura general en el lenguaje de programación C.

---
+++ EstructuraGeneral_C.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h> //cabecera estándar de entrada y salida de datos
+
+/*
+	Name: Hola mundo en C
+	Copyright: @KarydNa9
+	Author: Karina Bautista
+	Date: 07/12/21 18:15
+	Description: Estructura general de un programa en C
+*/
+
+int main(){ //función principal
+	
+	printf("Hola mundo :D\n"); // \n salto de línea
+	printf("Hola mundo :D");
+	
+	return 0; //Indica que el programa ha terminado
+}
