2902117e4f69a1a1feedb7ebb2839224a0d871ad
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-08 12:23:47 -0600

    Agregando mensajes en el menú.

--- Fundamentos/Tarea3.c
+++ Fundamentos/Tarea3.c
@@ -11,7 +11,7 @@
 
 void main(void){
 	int numero;
-	printf("\tMENU\n")
+	printf("\tMENU\n");
 	printf("Digite un numero entre (1-17): \n");
 	printf("1. a=b*c\n");
 	printf("2. d=(b^2)-4*(a*c) \n");
@@ -19,7 +19,7 @@ void main(void){
 	printf("4. i=x+y-z \n");
 	printf("5. j=(x+y)/(z+w) \n");
 	printf("6. k=5/(1+x^2) \n");
-	printf("7. r=(x/y)+1 \n");
+	printf("7. (x/y)+1 \n");
 	printf("8. (x+y)/(x-y) \n");
 	printf("9. x+(y/z) \n");
 	printf("10. z/(x-(y/z)) \n");
@@ -34,11 +34,41 @@ void main(void){
 	scanf("%i",&numero);
 	
 	switch(numero){
-		case 1: printf("\n Es el numero 1");
+		case 1: printf("\n 1. Resolver a=b*c \n");
 		break;
-		case 2: printf("\n Es el numero 2");
+		case 2: printf("\n 2. Resolver d=(b^2)-4*(a*c) \n");
 		break;
-		case 3: printf("\n Es el numero 3");
+		case 3: printf("\n 3. Resolver m=(y2-y1)/(x2-x1) \n");
+		break;
+		case 4: printf("\n 4. Resolver i=x+y-z \n");
+		break;
+		case 5: printf("\n 5. Resolver j=(x+y)/(z+w) \n");
+		break;
+		case 6: printf("\n 6. Resolver k=5/(1+x^2) \n");
+		break;
+		case 7: printf("\n 7. Resolver (x/y)+1 \n");
+		break;
+		case 8: printf("\n 8. Resolver (x+y)/(x-y) \n");
+		break;
+		case 9: printf("\n 9. Resolver x+(y/z) \n");
+		break;
+		case 10: printf("\n 10. Resolver z/(x-(y/z)) \n");
+		break;
+		case 11: printf("\n 11. Resolver b/(c+d) \n");
+		break;
+		case 12: printf("\n 12. Resolver (a+b)*(c/d) \n");
+		break;
+		case 13: printf("\n 13. Resolver ((a+b)^2)^2) \n");
+		break;
+		case 14: printf("\n 14. Resolver(x*y)/(1-4*(z*x)) \n");
+		break;
+		case 15: printf("\n 15. Resolver (x*y)/(m*n) \n");
+		break;
+		case 16: printf("\n 16. Resolver ((x+y)^2)*(a-b) \n");
+		break;
+		case 17: printf("\n 17. Resolver F and V and F or V \n");
+		break;
+		case 18: printf("\n 18. Resolver F and (F or V) and F or F \n");
 		break;
 		default: printf("No ha digitado un numero correcto");
 	}
