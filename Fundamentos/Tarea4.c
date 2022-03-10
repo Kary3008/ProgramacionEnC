416f205e62858a444b7d0aa49970d102dc277046
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-10 10:51:14 -0600

    Case 2. Promedio mensual.
    Al recibir los datos la clave del empleado y los 6 primeros sueldos del año, calcule el ingreso total semestral y el promedio mensual.

--- Fundamentos/Tarea4.c
+++ Fundamentos/Tarea4.c
@@ -18,14 +18,15 @@ void main(void){
 	int answer; 
 	do {
 		//Inicia el menu
-		int num, a, b, c, d, e;
+		int num, a, b, c, d, cla;
+		float su1, su2, su3, su4, su5, su6, ing, pm;
 			printf("\tMENU\n\n");
 			printf("1. Ejercicio 1: Orden Inverso\n");
 			printf("2. Ejercicio 2  \n");
 			printf("3. Ejercicio 3 \n");
 			printf("4. Ejercicio 4 \n");
 			printf("5. Ejercicio 5 \n");
-			printf("Digite una opción entre el 1 al 5: \n");
+			printf("Digite una opcion entre el 1 al 5: ");
 			scanf("%i",&num);
 		//Se introduce un switch case
 		switch(num) 
@@ -42,8 +43,35 @@ void main(void){
 					scanf("%i",&d);
 					printf("%i %i %i %i", d, c, b, a);	
 			break;
-			case 2: printf("\n\tTarea 2\n");
-			
+			case 2: printf("\n\tPromedio mensual\n");
+					printf("Al recibir los datos la clave del empleado y los 6 primeros sueldos del an¬o,\ncalcule el ingreso total semestral y el promedio mensual.\n");
+					/*
+					clave del empleado: cla -> int
+					6 sueldos del año: su1, su2, su3, su4, su5, su6 ->float.
+					*/
+					printf("Clave del empleado: ");
+					scanf("%i",&cla);
+					printf("Digite su primer sueldo del an¬o: \n");
+					scanf("%f",&su1);
+					printf("Digite su segundo sueldo del an¬o:  \n");
+					scanf("%f",&su2);
+					printf("Digite su tercer sueldo del an¬o:  \n");
+					scanf("%f",&su3);
+					printf("Digite su cuarto sueldo del an¬o: \n");
+					scanf("%f",&su4);
+					printf("Digite su quinto sueldo del an¬o:  \n");
+					scanf("%f",&su5);
+					printf("Digite su sexto sueldo del an¬o:  \n");
+					scanf("%f",&su6);
+					/*imprimir clave, ingreso total y promedio mensual*/
+					printf("\nClave del empleado: %i\n",cla);
+					//ingreso total
+					ing= su1+su2+su3+su4+su5+su6;
+					printf("Ingreso total semestral: $%.2f\n", ing);
+					//promedio mensual
+					pm= ing/6;
+					printf("Promedio mensual: %.2f\n", pm);
+					
 			break;
 			case 3: printf("\n\tTarea 3\n");
 			
