1d515a9ba50ca8cb1a0cdd3d2fe904d9795f2905
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-10 19:47:46 -0600

    Ejercicios mayor y menor.

--- Fundamentos/MayorMenor.c
+++ Fundamentos/MayorMenor.c
@@ -1,11 +1,64 @@
 #include<stdio.h>
 void main(void){
-	int a=4, b=6, c=8;
-	if(a>b>c){
-		printf("El resultado es: %i",a);
-		printf("El resultado medio: %i",b);
-		printf("El resultado menor: %i",c);
+	int a, b, c;
+	printf("Introduce 3 enteros diferentes entre si: ");
+	scanf("%i %i %i",&a, &b, &c);
+	if(a>b){
+		if(b>c){
+			printf("a>c>b");
+		}
+		else if(a>c){
+			printf("a>c>b");
+		}
+		else {
+			printf("c>a>b");
+		}
 	}
+	else{
+		if(a>c){
+			printf("b>a>c");
+		}
+		else if(b>c){
+			printf("b>c>a");
+		}
+		else{
+			printf("c>b>a");
+		}
+		
+	}
+	
+	
+	
+	/*
+	
+	
+	else(b>c){
+		printf("a>b>c");
+	}
+	else if(a>c){
+		printf("a>c>b");
+	}
+	else if{
+		printf("a>c>b");
+	}*/
+	
+	 /*
+	else if(b>a){
+		//printf("El resultado es: %i",a);
+	}
+	else if(a>c){
+		printf("b>a>c");
+	}
+	else if(b>c){
+		//printf("El resultado es: %i",a);
+	}
+	else if(a>c){
+		printf("b>a>c");
+	}
+	*/
+	
+	
+	/*
 	else if(a>c>b){
 		printf("El resultado es: %i",a);
 		printf("El resultado medio: %i",c);
@@ -30,5 +83,5 @@ void main(void){
 		printf("El resultado es: %i",c);
 		printf("El resultado medio: %i",b);
 		printf("El resultado menor: %i",a);
-	}
+	}*/
 }
