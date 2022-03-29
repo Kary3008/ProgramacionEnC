a57da4a406e3358b373928c5d239b47d246f2945
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-28 19:17:32 -0600

    Ejemplos de While

---
+++ Fundamentos/While.c
@@ -0,0 +1,31 @@
+/*
+	Name: WHILE	
+	Copyright: @KarydNa9
+	Author: Bautista Bautista Karina
+	Date: 28/03/22 19:04
+	Description: Ejemplos de secuencias con la estructura While.
+*/
+
+#include<stdio.h>
+
+void main(void){
+
+	//primera secuencia
+	int num=1;
+	
+	while(num<=5)
+	{
+		printf("%d\n",num);
+		num++;	
+	}
+	
+	//segunda secuencia
+	int nu=1;
+		
+	while(nu<=20)
+	{
+		printf("%d\n",nu);
+		num++;	
+	}
+	
+}
