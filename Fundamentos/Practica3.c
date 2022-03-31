a0f380c525699bf3e70b73d701fc621f3d8cb547
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-31 09:53:10 -0600

    Practica 3 sin operadores lógicos.

--- Fundamentos/Practica3.c
+++ Fundamentos/Practica3.c
@@ -9,17 +9,29 @@ int main()
     scanf("%d",&b);
     printf("\nc) ");
     scanf("%d",&c);
-    if (a<b && a<c && b<c)
-        printf("\nTus valores estan ordenados");   
-    if (b<a && b<c && a<c)
-        printf("\nTus valores tienen el orden b a c");
-    if (c<a && c<b && a<b)
-        printf("\nTus valores tienen el orden c a b");
-    if (b<c && b<a && c<a)
-        printf("\nTus valores tienen el orden b c a");
-    if (a<c && a<b && c<b)
-        printf("\nTus valores tienen el orden a c b");
-    if (c<b && c<a && b<a)
+    if (a<b)
+    	if(a<c)
+    		if(b<c)
+        		printf("\nTus valores estan ordenados");   
+    if (b<a)
+    	if(b<c)
+    		if(a<c)
+       			printf("\nTus valores tienen el orden b a c");
+    if (c<a)
+    	if(c<b)
+    		if(a<b)
+        		printf("\nTus valores tienen el orden c a b");
+    if (b<c)
+    	if(b<a)
+    		if(c<a)
+        		printf("\nTus valores tienen el orden b c a");
+    if (a<c)
+    	if(a<b)
+    		if(c<b)
+        		printf("\nTus valores tienen el orden a c b");
+    if (c<b)
+    	if(c<a)
+    		if(b<a)
         printf("\nTus valores tienen el orden c b a");
     return 0;
 }
