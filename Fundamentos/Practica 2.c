530df163c37509580475e3e750e63290fa3dfe68
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-03-31 09:37:42 -0600

    Practica 2
    Leer tres letras y encontrar y visualizar aquella que viene primero en el alfabeto.

--- Fundamentos/Practica 2.c
+++ Fundamentos/Practica 2.c
@@ -1,22 +1,13 @@
-/*
-	Name: Practica 10
-	Copyright: @KarydNa9
-	Author: Bautista Bautista Karina
-	Date: 30/03/22 23:54
-	Description: Hacer un menu con 10 programas, sin usar
-	operadores lógicos.
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 
 void main(void){
 	char a,b,c,letra;
-	printf("Dame la primera letra: ");
+	printf("Ingresa la primera letra: ");
 	scanf(" %c",&a);
-	printf("Dame la segunda letra: ");
+	printf("Ingresa la segunda letra: ");
 	scanf(" %c",&b);
-	printf("Dame la tercera letra: ");
+	printf("Ingresa la tercera letra: ");
 	scanf(" %c",&c);
 		if(a<b && a<c)
 			letra = a;
