054c791daae522cbe477238a1061d7797233b3c8
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-04-06 09:14:43 -0500

    Tarea 11, 12

---
+++ Fundamentos/Tarea11.c
@@ -0,0 +1,28 @@
+/*
+	Name: Romper por valor en While	
+	Copyright: @KarydNa9
+	Author: Bautista Bautista Karina
+	Date: 05/04/22 07:28
+	Description: Hacer un ciclo While que rompa el ciclo
+	por valor.
+*/
+#include<stdio.h>
+
+void main(void)
+{
+	float calif,tot=0,prom,numAls;
+	int i=1;
+	
+	printf("#num de calificaciones a capturar: ");
+	scanf("%f",&numAls);
+	while(i<=numAls) 
+	{
+		printf("Calif%i: ",i);
+		scanf("%f",&calif);
+		tot=tot+calif;
+		i++;
+	}
+	prom=tot/numAls;
+	printf("El promedio es: %.2f",prom);
+
+}
