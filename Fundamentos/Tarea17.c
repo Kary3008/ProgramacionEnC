ff4dff8c0fc14ec8bbd9e37bce495184b3aaf908
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-04-13 00:22:36 -0500

    Tarea 17 -S. ULAM

---
+++ Fundamentos/Tarea17.c
@@ -0,0 +1,19 @@
+/*
+	Name: 
+	Copyright: 
+	Author: 
+	Date: 13/04/22 00:18
+	Description: 
+*/
+
+#include<stdio.h>
+void main(void)
+{
+	int num;
+	printf("Ingrese un numero \n");
+	scanf("%d",&num);
+	if(num>0)
+		printf("%i",num);
+		while(num!=2)
+			
+}
