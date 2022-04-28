ecc9548cba03f3743b0cf40e5c243cfa67c2914d
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-04-28 11:39:51 -0500

    Binario a decimal.

---
+++ Fundamentos/bin.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main(void){
+	int n, bin[999]; //arreglo
+	int i=0;
+	
+	printf("Ingrese un numero decimal");
+	scanf("%i". &n);
+	
+	while(n!=0) //almacena valores de 0 y 1
+	{
+		bin[i]=n%2; //el numero se divide entre 2 y se almacena el residuo: (1,0)
+		n=n/2;
+		i++;
+	}
+	i--;
+	printf("Binario: ");
+	
+	while (i>=0)
+	{
+		printf("%i",bin[i]); //imprime los valores guardados en el arreglo
+		i--; //se imprime el arreglo de forma inversa
+	}
+}
