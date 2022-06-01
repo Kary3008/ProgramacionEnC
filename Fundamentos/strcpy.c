19f721a3576800cee5477b91ad956d0cab89daea
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-06-01 00:01:04 -0500

    Formas de strcpy

--- Fundamentos/strcpy.c
+++ Fundamentos/strcpy.c
@@ -5,14 +5,22 @@
 void main(void)
 {
 	char string1[20];
-	char string2[20];
+	char string2[10];
 	
-	printf("Ingrese una palabra: ");
+	printf("Ingrese una palabra para s1: ");
 	scanf("%s",string1);
 	
+	//devuelve la cantidad de caracteres sin contar \0 STRLEN()
+	int longitud = strlen(string1);
 	
+	if(longitud < 10)
+	{
 	strcpy(string2, string1);
-	
 	printf("La palabra en string1 es: %s", string1);
 	printf("\nLa palabra en string2 es: %s", string2);
+	}
+	else
+	{
+		printf("No se pudo copiar s1 a s2\n");
+	}
 }
