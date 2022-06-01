691fcc464bc941404a62dba579c5b1b44e9937c2
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-05-31 22:35:12 -0500

    Función Strcpy( )
    Copiar el contenido de una cadena a otra

--- Fundamentos/strcpy.c
+++ Fundamentos/strcpy.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//copiar el contenido de una cadena a otra - Funcion strcpy()
 void main(void)
 {
 	char string1[20];
 	char string2[20];
 	
-	printf();
+	printf("Ingrese una palabra: ");
 	scanf("%s",string1);
-
+	
+	
+	strcpy(string2, string1);
+	
+	printf("La palabra en string1 es: %s", string1);
+	printf("\nLa palabra en string2 es: %s", string2);
 }
