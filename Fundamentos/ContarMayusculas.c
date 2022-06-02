8bc862e92ddb3938c2a04b7f8856bc8375d07edc
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-06-02 10:13:10 -0500

    Error en programa contar Mayus

--- Fundamentos/ContarMayusculas.c
+++ Fundamentos/ContarMayusculas.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void main(void)
 {
 	char texto;
 	int mayus=0;
+	int vocal=0;
 	
+	do{
 	printf("Introduzca texto: ");
-	gets(texto);
+	scanf("%s",texto);
 	
-	if((texto>='A') && (texto<='Z'))
-		mayus++;
-	strcpy(mayus,texto);
-
-	strupr(mayus);
-	
+	switch(texto){
+		case 'a': case 'e': case 'i': case 'o': case 'u': 'A': case 'E': case 'I': case 'O': case 'U';
+		vocal++;
+		default: if((texto>='A') && (texto<='Z')) mayus++;
+//	strupr(mayus);
+		}
+	} while (letra!='.');
 	printf("Texto original: %s\n",texto);
-	printf("Mayusculas: %s\n", mayus);
+		printf("Mayusculas: %i\n", mayus);
+		printf("Vocales: %i\n", vocal);
 }
