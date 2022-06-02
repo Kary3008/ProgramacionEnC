218003a3e737a023d3f8909f6354f2d333ea6a7f
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-06-02 10:19:13 -0500

    Contar Mayusculas

--- Fundamentos/ContarMayusculas.c
+++ Fundamentos/ContarMayusculas.c
@@ -4,22 +4,36 @@
 
 void main(void)
 {
-	char texto;
+	/*char texto;
 	int mayus=0;
-	int vocal=0;
 	
 	do{
 	printf("Introduzca texto: ");
 	scanf("%s",texto);
 	
-	switch(texto){
-		case 'a': case 'e': case 'i': case 'o': case 'u': 'A': case 'E': case 'I': case 'O': case 'U';
-		vocal++;
-		default: if((texto>='A') && (texto<='Z')) mayus++;
-//	strupr(mayus);
+	if((texto>='A') && (texto<='Z')) mayus++;
 		}
-	} while (letra!='.');
+	while (texto!='.');
 	printf("Texto original: %s\n",texto);
 		printf("Mayusculas: %i\n", mayus);
-		printf("Vocales: %i\n", vocal);
+		*/
+	int contMayus = 0;
+	char palabra[20];
+	int i=0;
+	
+	printf("Ingresa una palabra: ");
+	scanf("%s", palabra);
+	
+	while(palabra[i]!='\0')
+	{
+		printf("\n%c", palabra[i]);
+		if(palabra[i]>='A'&&palabra[i]<='Z')
+		{
+			contMayus++;
+		}
+		i++;
+	}
+	
+	printf("\nLa cantidad de mayusculas de la palabra %s es: %d", palabra, contMayus);
+	
 }
