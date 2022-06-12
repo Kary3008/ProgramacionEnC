8cae861a132f28bb82a54a231c3fc36ec4880554
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-06-12 10:18:22 -0500

    Estructura

--- Fundamentos/Gato.c
+++ Fundamentos/Gato.c
@@ -1,17 +1,48 @@
+#include<stdlib.h>
 #include<stdio.h>
 
-void main(void)
+int main()
 {
-	char matriz[3][3]; //matriz principal
+	char matriz[3][3], opc; //matriz principal, opción
 	int i, j; //iteradores
+//	int fila, col, ganador;
+	
+	printf("\t Juego del gato\n");
+//	printf("Presione Y para jugar / N para salir: ");
+//	scanf("%c",&opc);
 	
 	for (i=0; i<3; i++)
-	{
-		for (j=0; j<3; j++){
-		matriz[i][j]='[ ]';
-		printf("%c", matriz[i][j]);
-		}
-		printf("\n");
-	}
+			{
+				for (j=0; j<3; j++){
+				matriz[i][j]=' ';
+				printf("[%c]", matriz[i][j]);
+				}
+				printf("\n");
+			}
+			
+	printf("Presione Y para jugar / N para salir: ");
+	scanf("%c",&opc);
+	int fila, col, ganador;		
 	
+	if(opc == 'y' || opc == 'Y'){
+		do{
+			printf("Jugador 1: \n");
+			printf("Digite fila: ");
+			scanf("%d", &fila);
+			printf("Digite columna: ");
+			scanf("%d", &col);
+			
+			matriz[fila][col] = 'x';
+			
+			for (i=0; i<3; i++)
+			{
+				for (j=0; j<3; j++){
+				matriz[i][j]=' ';
+				printf("[%c]", matriz[i][j]);
+				}
+				printf("\n");
+			}
+		} while(ganador != 1);
+	}
+	return 0;
 }
