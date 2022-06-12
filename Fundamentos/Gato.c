10a5942f6c0160c96f4fdf34f66c021e9d22436f
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-06-12 10:41:36 -0500

    Matriz funcional.

--- Fundamentos/Gato.c
+++ Fundamentos/Gato.c
@@ -5,39 +5,39 @@ int main()
 {
 	char matriz[3][3], opc; //matriz principal, opción
 	int i, j; //iteradores
-//	int fila, col, ganador;
+	int fila, col, ganador=0;
 	
 	printf("\t Juego del gato\n");
-//	printf("Presione Y para jugar / N para salir: ");
-//	scanf("%c",&opc);
+	printf("Presione S para jugar / N para salir: ");
+	scanf("%c", &opc);
 	
 	for (i=0; i<3; i++)
 			{
 				for (j=0; j<3; j++){
 				matriz[i][j]=' ';
-				printf("[%c]", matriz[i][j]);
+				printf("[%c]", matriz[i][j]); //imprime matriz
 				}
 				printf("\n");
 			}
 			
-	printf("Presione Y para jugar / N para salir: ");
-	scanf("%c",&opc);
-	int fila, col, ganador;		
+//	printf("Presione Y para jugar / N para salir: ");
+//	scanf("%c",&opc);
+//	int fila, col, ganador=0;		
 	
-	if(opc == 'y' || opc == 'Y'){
+	if(opc == 's' || opc == 'S'){
 		do{
 			printf("Jugador 1: \n");
-			printf("Digite fila: ");
+			printf("Digite fila: (0-2)");
 			scanf("%d", &fila);
-			printf("Digite columna: ");
+			printf("Digite columna: (0-2)");
 			scanf("%d", &col);
 			
 			matriz[fila][col] = 'x';
 			
+			//system("cls");
 			for (i=0; i<3; i++)
 			{
 				for (j=0; j<3; j++){
-				matriz[i][j]=' ';
 				printf("[%c]", matriz[i][j]);
 				}
 				printf("\n");
